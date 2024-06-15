0be7d878216ecfe295f8758031876c6648009c57
kumari-83 <jpuja6664@gmail.com>
2024-06-15 11:24:21 +0530

    Added script.js images and songs file

---
+++ js/music-list.js
@@ -0,0 +1,89 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Piya Tose - Qawwali",
+    artist: "Vylom",
+    img: "Piya Tose",
+    src: "Piya Tose"
+  },
+  {
+    name: "Mann Ki Lagan",
+    artist: "RFAK",
+    img: "Mann ki lagan",
+    src: "Mann ki lagan"
+  },
+  {
+    name: "Mera Mann",
+    artist: "Falak Sabir",
+    img: "Mera mann",
+    src: "Mera mann"
+  },
+  {
+    name: "Biba Sada Dil",
+    artist: "Madhur Sarma",
+    img: "Biba sada Dil",
+    src: "Biba sada Dil"
+  },
+  {
+    name: "Mileya Mileya",
+    artist: "Rekha Bhardwaj",
+    img: "Mileya Mileya",
+    src: "Mileya Mileya"
+  },
+  {
+    name: "Bol Na Halke Halke",
+    artist: "Karan Nawani",
+    img: "Bol na Halke",
+    src: "Bol na Halke"
+  },
+  {
+    name: "Ambarsariya",
+    artist: "Sona Mahapatra",
+    img: "Ambarsariya",
+    src: "Ambarsariya"
+  },
+  {
+    name: "Kuch To Hai",
+    artist: "Arman Malik",
+    img: "Kuch to hai",
+    src: "Kuch to hai"
+  },
+  {
+    name: "Hasta Hua - Qawwali",
+    artist: "Niazi Nizami Bros",
+    img: "Hasta hua qawaali",
+    src: "Hasta hua qawaali"
+  },
+  {
+    name: "Dagabaaz Re",
+    artist: "RFAK",
+    img: "Dagabaaz re",
+    src: "Dagabaaz re"
+  },
+  {
+    name: "Saiyyan",
+    artist: "Kailash Khair",
+    img: "Saiyyan",
+    src: "Saiyyan"
+  },
+
+
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
